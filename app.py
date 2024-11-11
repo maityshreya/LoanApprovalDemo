b1cafa447eb10c448af755bce931dbf00f76bdd8
Shreya Maity <124476788+maityshreya@users.noreply.github.com>
2024-11-11 14:15:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load pre-trained model
-model = joblib.load(r'C:\Users\DELL\RenderDUMMY\renderdemo\model.pkl')
+model = joblib.load(r'/model.pkl')
 
 # Home route to display the form
 @app.route('/')
